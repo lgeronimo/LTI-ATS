b28d52834be432e9ed62cfcb1d8a924db832a0e6
lgeronimo <luis.geronimov@gmail.com>
2024-11-23 04:52:41 -0600

    feat: add handle

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,12 +5,21 @@ import vacancyRoutes from './routes/vacancy.routes';
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://localhost:4200',
+  origin: process.env.FRONTEND_URL || 'http://localhost:4200' || 'https://localhost:4200',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 
 app.use('/api/vacancies', vacancyRoutes);
+
+// Middleware to handle not found routes
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: 'Ruta no encontrada',
+  });
+});
+
 
 export default app;
